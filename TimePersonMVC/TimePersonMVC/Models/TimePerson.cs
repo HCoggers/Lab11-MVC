88e71b9f9254b7c6a63695ee010f7a4a63d4dd6a
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-31 11:49:15 -0700

    bug fixed. style time!

--- TimePersonMVC/TimePersonMVC/Models/TimePerson.cs
+++ TimePersonMVC/TimePersonMVC/Models/TimePerson.cs
@@ -16,5 +16,43 @@ namespace TimePersonMVC.Models
         public string Title { get; set; }
         public string Category { get; set; }
         public string Context { get; set; }
+        public List<TimePerson> ReadPOTYFile(int start, int end)
+        {
+            String[] stringPeople = System.IO.File.ReadAllLines("C:/Users/harry/Desktop/code/week1-dotnet/Lab11-MVC/TimePersonMVC/TimePersonMVC/personOfTheYear.csv");
+            TimePerson[] timePeople = new TimePerson[stringPeople.Length - 1];
+
+            for (int i = 1; i < stringPeople.Length; i++)
+            {
+                string[] stringPerson = stringPeople[i].Split(",");
+
+                int year;
+                Int32.TryParse(stringPerson[0], out year);
+                int birth;
+                Int32.TryParse(stringPerson[4], out birth);
+                int death;
+                Int32.TryParse(stringPerson[5], out death);
+
+                timePeople[i - 1] = new TimePerson
+                {
+                    Year = year,
+                    Honor = stringPerson[1],
+                    Name = stringPerson[2],
+                    Country = stringPerson[3],
+                    BirthYear = birth,
+                    DeathYear = death,
+                    Title = stringPerson[6],
+                    Category = stringPerson[7],
+                    Context = stringPerson[8],
+                };
+            }
+
+            var people = from person in timePeople
+                         where person.Year >= start && person.Year <= end
+                         select person;
+
+            List<TimePerson> queriedPeople = people.ToList();
+
+            return queriedPeople;
+        }
     }
 }
